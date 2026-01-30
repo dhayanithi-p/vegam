5ad14948ecb9e1989160ab3c241bef02d39ae69e
Dhayanithi-P <dhayanithi709434@gmail.com>
2026-01-30 16:28:27 +0530

    feat: add Firebase initialization and export app, auth, and db instances

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,17 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID
+    apiKey: "AIzaSyAcDgPTMIDBdOCon3xT4kca0OnfbckKHL8",
+    authDomain: "vegam-114a0.firebaseapp.com",
+    projectId: "vegam-114a0",
+    storageBucket: "vegam-114a0.firebasestorage.app",
+    messagingSenderId: "408563321435",
+    appId: "1:408563321435:web:35fc89b21e42fed5ee4d7e"
 };
 
 // Initialize Firebase
